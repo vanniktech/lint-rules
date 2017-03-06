ef27807c3a42bedbbe54487f2c4e3249dde1f8ff
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-03-06 15:51:12 +0100

    RawColor: Ignore tools. (#17)

--- lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/RawColorDetectorTest.java
+++ lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/RawColorDetectorTest.java
@@ -6,12 +6,20 @@ import com.android.tools.lint.detector.api.Issue;
 import java.util.List;
 import org.intellij.lang.annotations.Language;
 
+import static com.vanniktech.lintrules.android.AndroidDetectorTest.NO_WARNINGS;
 import static com.vanniktech.lintrules.android.RawColorDetector.ISSUE_RAW_COLOR;
 import static java.util.Collections.singletonList;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class RawColorDetectorTest extends LintDetectorTest {
-  public void testTextColor() throws Exception {
+  public void testToolsTextColor() throws Exception {
+    @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+        + "<TextView xmlns:tools=\"http://schemas.android.com/tools\" tools:textColor=\"#fff\"/>\n";
+
+    assertThat(lintProject(xml("/res/layout/ids.xml", source))).isEqualTo(NO_WARNINGS);
+  }
+
+  public void testAndroidTextColor() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:textColor=\"#fff\"/>\n";
 
@@ -21,7 +29,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testTextColorHint() throws Exception {
+  public void testAndroidTextColorHint() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:textColorHint=\"#FFF\"/>\n";
 
@@ -31,7 +39,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testRippleColor() throws Exception {
+  public void testAndroidRippleColor() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:rippleColor=\"#FFF\"/>\n";
 
@@ -41,7 +49,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testItemTextColor() throws Exception {
+  public void testAndroidItemTextColor() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:itemTextColor=\"#FFF\"/>\n";
 
@@ -51,7 +59,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testCacheColorHint() throws Exception {
+  public void testAndroidCacheColorHint() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:cacheColorHint=\"#FFF\"/>\n";
 
@@ -61,7 +69,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testCardBackgroundColor() throws Exception {
+  public void testAndroidCardBackgroundColor() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:cardBackgroundColor=\"#FFF\"/>\n";
 
@@ -71,7 +79,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testTabIndicatorColor() throws Exception {
+  public void testAndroidTabIndicatorColor() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:tabIndicatorColor=\"#FFF\"/>\n";
 
@@ -81,7 +89,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testTabSelectedTextColor() throws Exception {
+  public void testAndroidTabSelectedTextColor() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:tabSelectedTextColor=\"#FFF\"/>\n";
 
@@ -91,7 +99,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testBackground() throws Exception {
+  public void testAndroidBackground() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:background=\"#FFF\"/>\n";
 
@@ -101,7 +109,7 @@ public class RawColorDetectorTest extends LintDetectorTest {
         + "0 errors, 1 warnings\n");
   }
 
-  public void testBackgroundTint() throws Exception {
+  public void testAndroidBackgroundTint() throws Exception {
     @Language("XML") final String source = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
         + "<TextView xmlns:android=\"http://schemas.android.com/apk/res/android\" android:backgroundTint=\"#FFF\"/>\n";
 
