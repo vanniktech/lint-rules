bd25f2943080cf98a431ffee8d687426c125e12a
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-04-06 15:00:43 +0200

    RawDimen: Ignore 0dp layout_widht & height when layout_weight is set. (#29)

--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/RawDimenDetector.java
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/RawDimenDetector.java
@@ -12,6 +12,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
+import static com.android.SdkConstants.ATTR_LAYOUT_HEIGHT;
+import static com.android.SdkConstants.ATTR_LAYOUT_WIDTH;
 import static com.android.resources.ResourceFolderType.DRAWABLE;
 import static com.android.resources.ResourceFolderType.LAYOUT;
 import static com.android.tools.lint.detector.api.Category.CORRECTNESS;
@@ -34,14 +36,17 @@ public final class RawDimenDetector extends LayoutDetector {
   @Override public void visitElement(final XmlContext context, final Element element) {
     final NamedNodeMap attributes = element.getAttributes();
 
+    final boolean hasLayoutWeight = attributes.getNamedItem("android:layout_weight") != null;
+
     for (int i = 0; i < attributes.getLength(); i++) {
       final Node item = attributes.item(i);
       final String value = item.getNodeValue();
 
       final boolean isToolsAttribute = "http://schemas.android.com/tools".equalsIgnoreCase(item.getNamespaceURI());
       final boolean isSuppressed = context.getDriver().isSuppressed(context, ISSUE_RAW_DIMEN, item);
+      final boolean is0DpLayoutWithWeight = hasLayoutWeight && value.startsWith("0") && (ATTR_LAYOUT_WIDTH.equals(item.getLocalName()) || ATTR_LAYOUT_HEIGHT.equals(item.getLocalName()));
 
-      if (value.matches("[\\d]+(sp|dp|dip)") && !isToolsAttribute && !isSuppressed) {
+      if (value.matches("[\\d]+(sp|dp|dip)") && !isToolsAttribute && !isSuppressed && !is0DpLayoutWithWeight) {
         context.report(ISSUE_RAW_DIMEN, context.getValueLocation((Attr) item), "Should be using dimen instead.");
       }
     }
