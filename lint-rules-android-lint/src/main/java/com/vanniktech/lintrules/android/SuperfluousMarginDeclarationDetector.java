3dd2a8e5b587a6ddc241e61ddbee739637f3a6dc
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-04-07 17:57:30 +0200

    Superfluous: Fix bug where margin + padding would be accumulated regardless of the parent.

--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/SuperfluousMarginDeclarationDetector.java
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/SuperfluousMarginDeclarationDetector.java
@@ -2,17 +2,21 @@ package com.vanniktech.lintrules.android;
 
 import com.android.annotations.NonNull;
 import com.android.resources.ResourceFolderType;
+import com.android.tools.lint.detector.api.Context;
 import com.android.tools.lint.detector.api.Implementation;
 import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.LayoutDetector;
 import com.android.tools.lint.detector.api.XmlContext;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
 
 import static com.android.SdkConstants.ATTR_LAYOUT_MARGIN_BOTTOM;
 import static com.android.SdkConstants.ATTR_LAYOUT_MARGIN_END;
@@ -28,7 +32,7 @@ public final class SuperfluousMarginDeclarationDetector extends LayoutDetector {
       "Instead of using start, end, bottom and top layout_margin can be used.", CORRECTNESS, 8, WARNING,
       new Implementation(SuperfluousMarginDeclarationDetector.class, RESOURCE_FILE_SCOPE));
 
-  private final Map<String, String> margins = new HashMap<>();
+  private final Map<Element, List<String>> margins = new HashMap<>();
 
   @Override public boolean appliesTo(@NonNull final ResourceFolderType folderType) {
     return folderType == LAYOUT;
@@ -44,18 +48,36 @@ public final class SuperfluousMarginDeclarationDetector extends LayoutDetector {
   }
 
   @Override public void visitAttribute(@NonNull final XmlContext context, @NonNull final Attr attribute) {
+    final Element ownerElement = attribute.getOwnerElement();
     final boolean isToolsAttribute = "http://schemas.android.com/tools".equalsIgnoreCase(attribute.getNamespaceURI());
     final boolean isSuppressed = context.getDriver().isSuppressed(context, SUPERFLUOUS_MARGIN_DECLARATION, attribute);
 
     if (!isToolsAttribute && !isSuppressed) {
-      margins.put(attribute.getName(), attribute.getValue());
+      final List<String> values = margins.get(ownerElement);
+
+      if (values != null) {
+        values.add(attribute.getValue());
+      } else {
+        final List<String> list = new ArrayList<>();
+        list.add(attribute.getValue());
+        margins.put(ownerElement, list);
+      }
     }
+  }
 
-    if (margins.size() == 4) {
-      final Set<String> values = new HashSet<>(margins.values());
+  @Override public void afterCheckFile(final Context context) {
+    final XmlContext xmlContext = (XmlContext) context;
 
-      if (values.size() == 1) {
-        context.report(SUPERFLUOUS_MARGIN_DECLARATION, context.getLocation(attribute.getOwnerElement()), "Should be using layout_margin instead.");
+    final Set<Map.Entry<Element, List<String>>> entries = margins.entrySet();
+    for (final Map.Entry<Element, List<String>> entry : entries) {
+      final List<String> marginsForElement = entry.getValue();
+
+      if (marginsForElement.size() == 4) {
+        final Set<String> values = new HashSet<>(marginsForElement);
+
+        if (values.size() == 1) {
+          context.report(SUPERFLUOUS_MARGIN_DECLARATION, xmlContext.getLocation(entry.getKey()), "Should be using layout_margin instead.");
+        }
       }
     }
   }
--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/SuperfluousPaddingDeclarationDetector.java
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/SuperfluousPaddingDeclarationDetector.java
@@ -2,17 +2,21 @@ package com.vanniktech.lintrules.android;
 
 import com.android.annotations.NonNull;
 import com.android.resources.ResourceFolderType;
+import com.android.tools.lint.detector.api.Context;
 import com.android.tools.lint.detector.api.Implementation;
 import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.LayoutDetector;
 import com.android.tools.lint.detector.api.XmlContext;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
 
 import static com.android.SdkConstants.ATTR_PADDING_BOTTOM;
 import static com.android.SdkConstants.ATTR_PADDING_END;
@@ -28,7 +32,7 @@ public final class SuperfluousPaddingDeclarationDetector extends LayoutDetector 
       "Instead of using start, end, bottom and top padding can be used.", CORRECTNESS, 8, WARNING,
       new Implementation(SuperfluousPaddingDeclarationDetector.class, RESOURCE_FILE_SCOPE));
 
-  private final Map<String, String> paddings = new HashMap<>();
+  private final Map<Element, List<String>> margins = new HashMap<>();
 
   @Override public boolean appliesTo(@NonNull final ResourceFolderType folderType) {
     return folderType == LAYOUT;
@@ -44,18 +48,36 @@ public final class SuperfluousPaddingDeclarationDetector extends LayoutDetector 
   }
 
   @Override public void visitAttribute(@NonNull final XmlContext context, @NonNull final Attr attribute) {
+    final Element ownerElement = attribute.getOwnerElement();
     final boolean isToolsAttribute = "http://schemas.android.com/tools".equalsIgnoreCase(attribute.getNamespaceURI());
     final boolean isSuppressed = context.getDriver().isSuppressed(context, SUPERFLUOUS_PADDING_DECLARATION, attribute);
 
     if (!isToolsAttribute && !isSuppressed) {
-      paddings.put(attribute.getName(), attribute.getValue());
+      final List<String> values = margins.get(ownerElement);
+
+      if (values != null) {
+        values.add(attribute.getValue());
+      } else {
+        final List<String> list = new ArrayList<>();
+        list.add(attribute.getValue());
+        margins.put(ownerElement, list);
+      }
     }
+  }
 
-    if (paddings.size() == 4) {
-      final Set<String> values = new HashSet<>(paddings.values());
+  @Override public void afterCheckFile(final Context context) {
+    final XmlContext xmlContext = (XmlContext) context;
 
-      if (values.size() == 1) {
-        context.report(SUPERFLUOUS_PADDING_DECLARATION, context.getLocation(attribute.getOwnerElement()), "Should be using padding instead.");
+    final Set<Map.Entry<Element, List<String>>> entries = margins.entrySet();
+    for (final Map.Entry<Element, List<String>> entry : entries) {
+      final List<String> paddingsForElement = entry.getValue();
+
+      if (paddingsForElement.size() == 4) {
+        final Set<String> values = new HashSet<>(paddingsForElement);
+
+        if (values.size() == 1) {
+          context.report(SUPERFLUOUS_PADDING_DECLARATION, xmlContext.getLocation(entry.getKey()), "Should be using padding instead.");
+        }
       }
     }
   }
