d1ec558db99c3692ec25bba141109177c7bb05b4
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-07-01 23:46:05 +0200

    Remove NonFinalClassDetector.

--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/IssueRegistry.java
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/IssueRegistry.java
@@ -6,16 +6,15 @@ import java.util.Collections;
 import java.util.List;
 
 import static com.vanniktech.lintrules.android.DefaultLayoutAttributeDetector.ISSUE_DEFAULT_LAYOUT_ATTRIBUTE;
+import static com.vanniktech.lintrules.android.InvalidSingleLineCommentDetector.ISSUE_INVALID_SINGLE_LINE_COMMENT;
 import static com.vanniktech.lintrules.android.InvalidStringDetector.ISSUE_INVALID_STRING;
 import static com.vanniktech.lintrules.android.MatchingMenuIdDetector.ISSUE_MATCHING_MENU_ID;
 import static com.vanniktech.lintrules.android.MatchingViewIdDetector.ISSUE_MATCHING_VIEW_ID;
-import static com.vanniktech.lintrules.android.NonFinalClassDetector.ISSUE_NON_FINAL_CLASS;
 import static com.vanniktech.lintrules.android.RawColorDetector.ISSUE_RAW_COLOR;
 import static com.vanniktech.lintrules.android.RawDimenDetector.ISSUE_RAW_DIMEN;
 import static com.vanniktech.lintrules.android.ShouldUseStaticImportDetector.ISSUE_SHOULD_USE_STATIC_IMPORT;
 import static com.vanniktech.lintrules.android.SuperfluousMarginDeclarationDetector.SUPERFLUOUS_MARGIN_DECLARATION;
 import static com.vanniktech.lintrules.android.SuperfluousPaddingDeclarationDetector.SUPERFLUOUS_PADDING_DECLARATION;
-import static com.vanniktech.lintrules.android.InvalidSingleLineCommentDetector.ISSUE_INVALID_SINGLE_LINE_COMMENT;
 import static com.vanniktech.lintrules.android.WrongConstraintLayoutUsageDetector.ISSUE_WRONG_CONSTRAINT_LAYOUT_USAGE;
 import static com.vanniktech.lintrules.android.WrongMenuIdFormatDetector.ISSUE_WRONG_MENU_ID_FORMAT;
 import static com.vanniktech.lintrules.android.WrongViewIdFormatDetector.ISSUE_WRONG_VIEW_ID_FORMAT;
@@ -24,7 +23,6 @@ public final class IssueRegistry extends com.android.tools.lint.client.api.Issue
   @Override public List<Issue> getIssues() {
     final List<Issue> issues = new ArrayList<>();
     Collections.addAll(issues, AndroidDetector.getIssues());
-    issues.add(ISSUE_NON_FINAL_CLASS);
     issues.add(ISSUE_WRONG_VIEW_ID_FORMAT);
     issues.add(ISSUE_WRONG_MENU_ID_FORMAT);
     issues.add(ISSUE_RAW_DIMEN);
