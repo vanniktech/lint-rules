5d945a9a343c27a1af69e1f62723d9a31d741655
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-12-22 23:31:45 +0100

    Add NamingPatternDetector. (#128)

---
+++ lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/NamingPatternDetectorTest.kt
@@ -0,0 +1,72 @@
+package com.vanniktech.lintrules.android
+
+import com.android.tools.lint.checks.infrastructure.TestFiles.java
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.Test
+
+class NamingPatternDetectorTest {
+  @Test fun incorrectVariableName() {
+    lint()
+      .files(java("""
+          |package foo;
+          |
+          |class Foo {
+          |  private void fun() {
+          |    String iOSVersion;
+          |  }
+          |}""".trimMargin()))
+      .issues(ISSUE_NAMING_PATTERN)
+      .run()
+      .expect("""
+          |src/foo/Foo.java:5: Warning: Not named in defined camel case. [NamingPattern]
+          |    String iOSVersion;
+          |    ~~~~~~~~~~~~~~~~~~
+          |0 errors, 1 warnings""".trimMargin())
+  }
+
+  @Test fun incorrectMethodName() {
+    lint()
+      .files(java("""
+          |package foo;
+          |
+          |class Foo {
+          |  private void makeHTTPRequest() {
+          |  }
+          |}""".trimMargin()))
+      .issues(ISSUE_NAMING_PATTERN)
+      .run()
+      .expect("""
+          |src/foo/Foo.java:4: Warning: Not named in defined camel case. [NamingPattern]
+          |  private void makeHTTPRequest() {
+          |               ~~~~~~~~~~~~~~~
+          |0 errors, 1 warnings""".trimMargin())
+  }
+
+  @Test fun correctClassName() {
+    lint()
+      .files(java("""
+          |package foo;
+          |
+          |class XmlHttpRequest {
+          |}""".trimMargin()))
+      .issues(ISSUE_NAMING_PATTERN)
+      .run()
+      .expectClean()
+  }
+
+  @Test fun incorrectClassName() {
+    lint()
+      .files(java("""
+          |package foo;
+          |
+          |class XMLHTTPRequest {
+          |}""".trimMargin()))
+      .issues(ISSUE_NAMING_PATTERN)
+      .run()
+      .expect("""
+          |src/foo/XMLHTTPRequest.java:3: Warning: Not named in defined camel case. [NamingPattern]
+          |class XMLHTTPRequest {
+          |      ~~~~~~~~~~~~~~
+          |0 errors, 1 warnings""".trimMargin())
+  }
+}
