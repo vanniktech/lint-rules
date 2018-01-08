5134f3b34ef1e2bc629dd4bbe018f6ef9982b383
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-08 22:02:39 +0100

    InvalidSingleLineCommentDetector: Also run on Gradle files.

--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/InvalidSingleLineCommentDetector.java
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/InvalidSingleLineCommentDetector.java
@@ -4,7 +4,6 @@ import com.android.tools.lint.detector.api.Context;
 import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Implementation;
 import com.android.tools.lint.detector.api.Issue;
-import com.android.tools.lint.detector.api.JavaContext;
 import com.android.tools.lint.detector.api.LintFix;
 import com.android.tools.lint.detector.api.Location;
 import java.net.MalformedURLException;
@@ -19,27 +18,26 @@ import org.jetbrains.uast.UClass;
 import org.jetbrains.uast.UElement;
 
 import static com.android.tools.lint.detector.api.Category.CORRECTNESS;
+import static com.android.tools.lint.detector.api.Scope.GRADLE_FILE;
 import static com.android.tools.lint.detector.api.Scope.JAVA_FILE;
 import static com.android.tools.lint.detector.api.Scope.TEST_SOURCES;
 import static com.android.tools.lint.detector.api.Severity.WARNING;
 
-public final class InvalidSingleLineCommentDetector extends Detector implements Detector.UastScanner {
+public final class InvalidSingleLineCommentDetector extends Detector implements Detector.UastScanner, Detector.GradleScanner {
   private static final String COMMENT = "//";
 
   static final Issue ISSUE_INVALID_SINGLE_LINE_COMMENT = Issue.create("InvalidSingleLineComment",
       "Marks single line comments that are not sentences.",
       "Single line comments should always be sentences. They're part of the code and hence they deserve as much detail and respect as code.",
       CORRECTNESS, 8, WARNING,
-      new Implementation(InvalidSingleLineCommentDetector.class, EnumSet.of(JAVA_FILE, TEST_SOURCES)));
+      new Implementation(InvalidSingleLineCommentDetector.class, EnumSet.of(JAVA_FILE, TEST_SOURCES, GRADLE_FILE)));
 
   @Override public List<Class<? extends UElement>> getApplicableUastTypes() {
     return Collections.<Class<? extends UElement>>singletonList(UClass.class);
   }
 
   @Override @SuppressWarnings("PMD.CyclomaticComplexity") public void afterCheckFile(final Context context) {
-    final JavaContext javaContext = (JavaContext) context;
-
-    final String source = String.valueOf(javaContext.getContents());
+    final String source = String.valueOf(context.getContents());
 
     final Pattern compile = Pattern.compile("[\\t]*" + COMMENT + ".*");
     final Matcher matcher = compile.matcher(source);
