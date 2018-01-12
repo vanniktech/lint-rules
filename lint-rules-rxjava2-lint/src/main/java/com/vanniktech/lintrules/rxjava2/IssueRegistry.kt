d850cb897f5925bf82bac5dc8d90ea6ab99c9c51
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-12 13:24:56 +0100

    RxJava2MethodMissingCheckReturnValueDetector: Add quickfix. (#155)

--- lint-rules-rxjava2-lint/src/main/java/com/vanniktech/lintrules/rxjava2/IssueRegistry.kt
+++ lint-rules-rxjava2-lint/src/main/java/com/vanniktech/lintrules/rxjava2/IssueRegistry.kt
@@ -1,6 +1,5 @@
 package com.vanniktech.lintrules.rxjava2
 
-import com.vanniktech.lintrules.rxjava2.RxJava2MethodMissingCheckReturnValueDetector.ISSUE_METHOD_MISSING_CHECK_RETURN_VALUE
 import com.vanniktech.lintrules.rxjava2.RxJava2MissingCompositeDisposableClearDetector.ISSUE_MISSING_COMPOSITE_DISPOSABLE_CLEAR
 
 class IssueRegistry : com.android.tools.lint.client.api.IssueRegistry() {
