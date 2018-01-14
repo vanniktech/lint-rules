4f65d70945c03aa2787ef43be385caf89172e0f6
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-14 19:04:01 +0100

    RawDimenDetector & RawColorDetector: Add quickfixes. (#161)

--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/RawColorDetector.kt
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/RawColorDetector.kt
@@ -1,9 +1,14 @@
 package com.vanniktech.lintrules.android
 
+import com.android.SdkConstants.ATTR_COLOR
+import com.android.SdkConstants.ATTR_PATH
+import com.android.SdkConstants.TAG_VECTOR
 import com.android.resources.ResourceFolderType
 import com.android.resources.ResourceFolderType.DRAWABLE
 import com.android.resources.ResourceFolderType.LAYOUT
+import com.android.resources.ResourceFolderType.VALUES
 import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
+import com.android.tools.lint.detector.api.Context
 import com.android.tools.lint.detector.api.Implementation
 import com.android.tools.lint.detector.api.Issue
 import com.android.tools.lint.detector.api.ResourceXmlDetector
@@ -21,16 +26,34 @@ val ISSUE_RAW_COLOR = Issue.create("RawColor",
     Implementation(RawColorDetector::class.java, RESOURCE_FILE_SCOPE))
 
 class RawColorDetector : ResourceXmlDetector() {
-  override fun appliesTo(folderType: ResourceFolderType) = EnumSet.of(LAYOUT, DRAWABLE).contains(folderType)
+  private var collector = ElementCollectReporter(ATTR_COLOR)
+
+  override fun appliesTo(folderType: ResourceFolderType) = EnumSet.of(LAYOUT, DRAWABLE, VALUES).contains(folderType)
 
   override fun getApplicableElements() = ALL
 
+  override fun beforeCheckProject(context: Context) = beforeCheckLibraryProject(context)
+
+  override fun beforeCheckLibraryProject(context: Context) {
+    collector = ElementCollectReporter(ATTR_COLOR)
+  }
+
   override fun visitElement(context: XmlContext, element: Element) {
+    collector.collect(element)
+
     (0 until element.attributes.length)
       .map { element.attributes.item(it) }
-      .filterNot { "vector" == element.localName || "path" == element.localName }
-      .filterNot { "http://schemas.android.com/tools".equals(it.namespaceURI, ignoreCase = true) }
+      .filterNot { TAG_VECTOR == element.localName || ATTR_PATH == element.localName }
+      .filterNot { it.hasToolsNamespace() }
       .filter { it.nodeValue.matches("#[a-fA-F\\d]{3,8}".toRegex()) }
-      .forEach { context.report(ISSUE_RAW_COLOR, it, context.getValueLocation(it as Attr), "Should be using a color resource instead.") }
+      .filterNot { context.driver.isSuppressed(context, ISSUE_RAW_COLOR, it) }
+      .map { it to context.getValueLocation(it as Attr) }
+      .toCollection(collector)
+  }
+
+  override fun afterCheckProject(context: Context) = afterCheckLibraryProject(context)
+
+  override fun afterCheckLibraryProject(context: Context) {
+    collector.report(ISSUE_RAW_COLOR, context, "Should be using a color resource instead.")
   }
 }
--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/RawDimenDetector.kt
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/RawDimenDetector.kt
@@ -3,10 +3,13 @@ package com.vanniktech.lintrules.android
 import com.android.SdkConstants.ATTR_LAYOUT_HEIGHT
 import com.android.SdkConstants.ATTR_LAYOUT_WIDTH
 import com.android.SdkConstants.CLASS_CONSTRAINT_LAYOUT
+import com.android.SdkConstants.TAG_DIMEN
 import com.android.resources.ResourceFolderType
 import com.android.resources.ResourceFolderType.DRAWABLE
 import com.android.resources.ResourceFolderType.LAYOUT
+import com.android.resources.ResourceFolderType.VALUES
 import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
+import com.android.tools.lint.detector.api.Context
 import com.android.tools.lint.detector.api.Implementation
 import com.android.tools.lint.detector.api.Issue
 import com.android.tools.lint.detector.api.ResourceXmlDetector
@@ -24,11 +27,21 @@ val ISSUE_RAW_DIMEN = Issue.create("RawDimen",
     Implementation(RawDimenDetector::class.java, RESOURCE_FILE_SCOPE))
 
 class RawDimenDetector : ResourceXmlDetector() {
-  override fun appliesTo(folderType: ResourceFolderType) = EnumSet.of(LAYOUT, DRAWABLE).contains(folderType)
+  private var collector = ElementCollectReporter(TAG_DIMEN)
+
+  override fun appliesTo(folderType: ResourceFolderType) = EnumSet.of(LAYOUT, DRAWABLE, VALUES).contains(folderType)
 
   override fun getApplicableElements() = ALL
 
+  override fun beforeCheckProject(context: Context) = beforeCheckLibraryProject(context)
+
+  override fun beforeCheckLibraryProject(context: Context) {
+    collector = ElementCollectReporter(TAG_DIMEN)
+  }
+
   override fun visitElement(context: XmlContext, element: Element) {
+    collector.collect(element)
+
     val hasLayoutWeight = element.attributes.getNamedItem("android:layout_weight") != null
     val isParentConstraintLayout = element.hasParent(CLASS_CONSTRAINT_LAYOUT)
     val isVectorGraphic = "vector" == element.localName || "path" == element.localName
@@ -39,6 +52,14 @@ class RawDimenDetector : ResourceXmlDetector() {
         .filterNot { isVectorGraphic }
         .filterNot { (hasLayoutWeight || isParentConstraintLayout) && it.nodeValue[0] == '0' && (ATTR_LAYOUT_WIDTH == it.localName || ATTR_LAYOUT_HEIGHT == it.localName) }
         .filter { it.nodeValue.matches("-?[\\d.]+(sp|dp|dip)".toRegex()) }
-        .forEach { context.report(ISSUE_RAW_DIMEN, it, context.getValueLocation(it as Attr), "Should be using a dimension resource instead.") }
+        .filterNot { context.driver.isSuppressed(context, ISSUE_RAW_DIMEN, it) }
+        .map { it to context.getValueLocation(it as Attr) }
+        .toCollection(collector)
+  }
+
+  override fun afterCheckProject(context: Context) = afterCheckLibraryProject(context)
+
+  override fun afterCheckLibraryProject(context: Context) {
+    collector.report(ISSUE_RAW_DIMEN, context, "Should be using a dimension resource instead.")
   }
 }
