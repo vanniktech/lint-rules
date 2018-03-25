fcf388ed924dcbcbcc4ea9b6575c12ff7daf3cd2
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-03-25 13:21:34 +0200

    Add UnusedMergeAttributesDetector. (#175)

---
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/UnusedMergeAttributesDetector.kt
@@ -0,0 +1,41 @@
+package com.vanniktech.lintrules.android
+
+import com.android.SdkConstants.ATTR_PARENT_TAG
+import com.android.SdkConstants.TOOLS_URI
+import com.android.SdkConstants.VIEW_MERGE
+import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.LayoutDetector
+import com.android.tools.lint.detector.api.Scope.Companion.RESOURCE_FILE_SCOPE
+import com.android.tools.lint.detector.api.Severity.WARNING
+import com.android.tools.lint.detector.api.XmlContext
+import org.w3c.dom.Element
+
+val ISSUE_UNUSED_MERGE_ATTRIBUTES = Issue.create("UnusedMergeAttributes",
+    "Flags android and app attributes that are used on a <merge> attribute for custom Views.",
+    "Adding android, app and other attributes to <merge> won't be used by the system for custom views and hence can lead to errors.",
+    CORRECTNESS, PRIORITY, WARNING,
+    Implementation(UnusedMergeAttributesDetector::class.java, RESOURCE_FILE_SCOPE))
+
+class UnusedMergeAttributesDetector : LayoutDetector() {
+  override fun getApplicableElements() = listOf(VIEW_MERGE)
+
+  override fun visitElement(context: XmlContext, element: Element) {
+    val hasParentTag = element.getAttributeNS(TOOLS_URI, ATTR_PARENT_TAG).isNotEmpty()
+
+    if (hasParentTag) {
+      element.attributes()
+          .filterNot { it.hasToolsNamespace() }
+          .filterNot { it.prefix == "xmlns" }
+          .forEach {
+            val fix = fix().name("Change to tools").composite(
+                fix().set(TOOLS_URI, it.localName, it.nodeValue).build(),
+                fix().unset(it.namespaceURI, it.localName).build()
+            )
+
+            context.report(ISSUE_UNUSED_MERGE_ATTRIBUTES, it, context.getLocation(it), "Attribute won't be used", fix)
+          }
+    }
+  }
+}
---
+++ lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/UnusedMergeAttributesDetectorTest.kt
@@ -0,0 +1,67 @@
+package com.vanniktech.lintrules.android
+
+import com.android.tools.lint.checks.infrastructure.TestFiles.xml
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.Test
+
+class UnusedMergeAttributesDetectorTest {
+  @Test fun androidAttribute() {
+    lint()
+        .files(xml("res/layout/view_custom.xml", """
+            |<merge
+            |    xmlns:android="http://schemas.android.com/apk/res/android"
+            |    xmlns:tools="http://schemas.android.com/tools"
+            |    android:layout_marginTop="64dp"
+            |    tools:parentTag="android.support.constraint.ConstraintLayout"
+            |    />""".trimMargin()))
+        .issues(ISSUE_UNUSED_MERGE_ATTRIBUTES)
+        .run()
+        .expect("""
+            |res/layout/view_custom.xml:4: Warning: Attribute won't be used [UnusedMergeAttributes]
+            |    android:layout_marginTop="64dp"
+            |    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            |0 errors, 1 warnings""".trimMargin())
+        .expectFixDiffs("""
+            |Fix for res/layout/view_custom.xml line 3: Change to tools:
+            |@@ -4 +4
+            |-     android:layout_marginTop="64dp"
+            |+     tools:layout_marginTop="64dp"
+            |""".trimMargin())
+  }
+
+  @Test fun appAttribute() {
+    lint()
+        .files(xml("res/layout/view_custom.xml", """
+            |<merge
+            |    xmlns:app="http://schemas.android.com/apk/res/android"
+            |    xmlns:tools="http://schemas.android.com/tools"
+            |    app:layout_marginTop="64dp"
+            |    tools:parentTag="android.support.constraint.ConstraintLayout"
+            |    />""".trimMargin()))
+        .issues(ISSUE_UNUSED_MERGE_ATTRIBUTES)
+        .run()
+        .expect("""
+            |res/layout/view_custom.xml:4: Warning: Attribute won't be used [UnusedMergeAttributes]
+            |    app:layout_marginTop="64dp"
+            |    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            |0 errors, 1 warnings""".trimMargin())
+        .expectFixDiffs("""
+            |Fix for res/layout/view_custom.xml line 3: Change to tools:
+            |@@ -4 +4
+            |-     app:layout_marginTop="64dp"
+            |+     tools:layout_marginTop="64dp"
+            |""".trimMargin())
+  }
+
+  @Test fun ignoreMergeWithoutParentTag() {
+    lint()
+        .files(xml("res/layout/view_custom.xml", """
+            |<merge
+            |    xmlns:app="http://schemas.android.com/apk/res/android"
+            |    app:layout_marginTop="64dp"
+            |    />""".trimMargin()))
+        .issues(ISSUE_UNUSED_MERGE_ATTRIBUTES)
+        .run()
+        .expectClean()
+  }
+}
