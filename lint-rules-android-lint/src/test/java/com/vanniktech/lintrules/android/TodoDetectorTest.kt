080184c41f2ddd993d9ffd1927f9c8ca65a81fff
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-03-25 14:37:59 +0200

    Add TodoDetector. (#177)

---
+++ lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/TodoDetectorTest.kt
@@ -0,0 +1,73 @@
+package com.vanniktech.lintrules.android
+
+import com.android.tools.lint.checks.infrastructure.TestFiles.gradle
+import com.android.tools.lint.checks.infrastructure.TestFiles.java
+import com.android.tools.lint.checks.infrastructure.TestFiles.manifest
+import com.android.tools.lint.checks.infrastructure.TestFiles.xml
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.Test
+
+class TodoDetectorTest {
+  @Test fun javaFile() {
+    lint()
+        .files(java("""
+            |package foo;
+            |
+            |class Example {
+            |  // TODO: something
+            |}""".trimMargin()))
+        .issues(ISSUE_TODO)
+        .run()
+        .expect("""
+            |src/foo/Example.java:4: Error: Contains todo. [Todo]
+            |  // TODO: something
+            |     ~~~~~~~~~~~~~~~
+            |1 errors, 0 warnings""".trimMargin())
+  }
+
+  @Test fun layoutFile() {
+    lint()
+        .files(xml("res/layout/layout.xml", """
+            |<TextView xmlns:android="http://schemas.android.com/apk/res/android"
+            |    android:text="Blub!"/>
+            |<!-- TODO: Fix blub. -->""".trimMargin()))
+        .issues(ISSUE_TODO)
+        .run()
+        .expect("""
+            |res/layout/layout.xml:3: Error: Contains todo. [Todo]
+            |<!-- TODO: Fix blub. -->
+            |     ~~~~~~~~~~~~~~~~~~~
+            |1 errors, 0 warnings""".trimMargin())
+  }
+
+  @Test fun gradleFile() {
+    lint()
+        .files(gradle("""
+            |buildscript {
+            |  repositories {
+            |    mavenCentral() // TODO: we should remove it.
+            |  }
+            |}""".trimMargin()))
+        .issues(ISSUE_TODO)
+        .run()
+        .expect("""
+            |build.gradle:3: Error: Contains todo. [Todo]
+            |    mavenCentral() // TODO: we should remove it.
+            |                      ~~~~~~~~~~~~~~~~~~~~~~~~~~
+            |1 errors, 0 warnings""".trimMargin())
+  }
+
+  @Test fun manifestFile() {
+    lint()
+        .files(manifest().withSource("""
+            |<!-- TODO: Something. -->
+            |<manifest package="com.vanniktech.lintrulesandroid"/>""".trimMargin()))
+        .issues(ISSUE_TODO)
+        .run()
+        .expect("""
+            |AndroidManifest.xml:1: Error: Contains todo. [Todo]
+            |<!-- TODO: Something. -->
+            |     ~~~~~~~~~~~~~~~~~~~~
+            |1 errors, 0 warnings""".trimMargin())
+  }
+}
