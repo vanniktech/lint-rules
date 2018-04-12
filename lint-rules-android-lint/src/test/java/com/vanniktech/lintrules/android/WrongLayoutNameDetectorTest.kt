d04fd7729cb27725cabc002ad119fce3e48aba33
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-04-12 11:32:46 +0200

    WrongLayoutNameDetector: Test resource prefix. (#193)

--- lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/WrongLayoutNameDetectorTest.kt
+++ lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/WrongLayoutNameDetectorTest.kt
@@ -1,5 +1,6 @@
 package com.vanniktech.lintrules.android
 
+import com.android.tools.lint.checks.infrastructure.TestFiles.gradle
 import com.android.tools.lint.checks.infrastructure.TestFiles.xml
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
 import org.junit.Test
@@ -8,6 +9,14 @@ class WrongLayoutNameDetectorTest {
   @Test fun activityFile() {
     lint()
         .files(xml("res/layout/activity_home.xml", "<merge/>"))
+        .issues(ISSUE_WRONG_LAYOUT_NAME)
+        .run()
+        .expectClean()
+  }
+
+  @Test fun activityFileWithResourcePrefix() {
+    lint()
+        .files(xml("res/layout/unit_test_prefix_activity_home.xml", "<merge/>"), resourcePrefix("unit_test_prefix_"))
         .issues(ISSUE_WRONG_LAYOUT_NAME)
         .run()
         .expectClean()
@@ -23,6 +32,16 @@ class WrongLayoutNameDetectorTest {
           |0 errors, 1 warnings""".trimMargin())
   }
 
+  @Test fun randomLayoutFileWithResourcePrefix() {
+    lint()
+        .files(xml("res/layout/random.xml", "<merge/>"), resourcePrefix("unit_test_prefix_"))
+        .issues(ISSUE_WRONG_LAYOUT_NAME)
+        .run()
+        .expect("""
+          |res/layout/random.xml: Warning: Layout does not start with one of the following prefixes: unit_test_prefix_activity_, unit_test_prefix_view_, unit_test_prefix_fragment_, unit_test_prefix_dialog_, unit_test_prefix_bottom_sheet_, unit_test_prefix_adapter_item_, unit_test_prefix_divider_, unit_test_prefix_space_ [WrongLayoutName]
+          |0 errors, 1 warnings""".trimMargin())
+  }
+
   @Test fun ignoreColorFile() {
     lint()
         .files(xml("res/values/color.xml", "<resources/>"))
@@ -31,3 +50,10 @@ class WrongLayoutNameDetectorTest {
         .expectClean()
   }
 }
+
+fun resourcePrefix(prefix: String) = gradle("""
+        apply plugin: 'com.android.library'
+
+        android {
+          resourcePrefix '$prefix'
+        }""").indented()
