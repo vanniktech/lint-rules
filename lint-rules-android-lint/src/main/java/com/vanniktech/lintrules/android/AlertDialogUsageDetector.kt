b27e8e1e46b4e2767e471be609a0c5bc3fc167a6
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-10-16 23:52:04 +0200

    Add AlertDialogUsageDetector. (#243)

---
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/AlertDialogUsageDetector.kt
@@ -0,0 +1,44 @@
+package com.vanniktech.lintrules.android
+
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Scope.JAVA_FILE
+import com.android.tools.lint.detector.api.Scope.TEST_SOURCES
+import com.android.tools.lint.detector.api.Severity.WARNING
+import com.intellij.psi.PsiType
+import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.UVariable
+import java.util.EnumSet
+
+val ISSUE_ALERT_DIALOG_USAGE = Issue.create("AlertDialogUsage",
+    "Use the support library AlertDialog instead of android.app.AlertDialog.",
+    "Support library AlertDialog is much more powerful and plays better together with the new theming / styling than the AlertDialog built into the framework.",
+    CORRECTNESS, PRIORITY, WARNING,
+    Implementation(AlertDialogUsageDetector::class.java, EnumSet.of(JAVA_FILE, TEST_SOURCES)))
+
+private const val FQDN_ANDROID_ALERT_DIALOG = "android.app.AlertDialog"
+
+class AlertDialogUsageDetector : Detector(), Detector.UastScanner {
+  override fun getApplicableUastTypes() = listOf<Class<out UElement>>(UVariable::class.java, UClass::class.java)
+
+  override fun createUastHandler(context: JavaContext) = AlertDialogUsageHandler(context)
+
+  class AlertDialogUsageHandler(
+    private val context: JavaContext
+  ) : UElementHandler() {
+    override fun visitVariable(node: UVariable) = process(node.type, node)
+
+    override fun visitClass(node: UClass) = node.uastSuperTypes.forEach { process(it.type, it) }
+
+    private fun process(type: PsiType, node: UElement) {
+      if (context.evaluator.typeMatches(type, FQDN_ANDROID_ALERT_DIALOG)) {
+        context.report(ISSUE_ALERT_DIALOG_USAGE, node, context.getLocation(node), "Should not be using android.app.AlertDialog.")
+      }
+    }
+  }
+}
--- lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/IssueRegistryTest.kt
+++ lint-rules-android-lint/src/test/java/com/vanniktech/lintrules/android/IssueRegistryTest.kt
@@ -30,6 +30,7 @@ class IssueRegistryTest {
         .joinToString(separator = "\n") { "- **${it.id}** - ${it.getExplanation(RAW)}" }
 
     assertEquals("""
+        - **AlertDialogUsage** - Support library AlertDialog is much more powerful and plays better together with the new theming / styling than the AlertDialog built into the framework.
         - **AssertjImport** - Importing org.assertj.core.api.Assertions is not ideal. Since it can require Java 8. It's simple as instead org.assertj.core.api.Java6Assertions can be imported and provides guarantee to run on Java 6 as well.
         - **ColorCasing** - Colors should have lowercase letters. #ff0099 is valid while #FF0099 isn't since the FF should be written in lower case.
         - **ConstraintLayoutToolsEditorAttribute** - The tools:layout_editor xml properties are only used for previewing and won't be used in your APK hence they're unnecessary and just add overhead.
