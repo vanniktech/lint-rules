465a098f7779b04595e17a08c97a21a1c9db9568
Tahsin Dane <sdane@wayfair.com>
2019-03-30 12:41:07 +0100

    Make MenuIdDetector lintFix more intelligent.
    Modified tests with new behavior of the fix.
    Also added new test with a simpler case.

--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/MatchingMenuIdDetector.kt
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/MatchingMenuIdDetector.kt
@@ -3,16 +3,12 @@ package com.vanniktech.lintrules.android
 import com.android.SdkConstants.ATTR_ID
 import com.android.resources.ResourceFolderType
 import com.android.resources.ResourceFolderType.MENU
+import com.android.tools.lint.detector.api.*
 import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
-import com.android.tools.lint.detector.api.Implementation
-import com.android.tools.lint.detector.api.Issue
-import com.android.tools.lint.detector.api.ResourceXmlDetector
 import com.android.tools.lint.detector.api.Scope.Companion.RESOURCE_FILE_SCOPE
 import com.android.tools.lint.detector.api.Severity.WARNING
-import com.android.tools.lint.detector.api.XmlContext
-import com.android.tools.lint.detector.api.stripIdPrefix
 import org.w3c.dom.Attr
-import java.util.EnumSet
+import java.util.*
 
 val ISSUE_MATCHING_MENU_ID = Issue.create("MatchingMenuId", "Flags menu ids that don't match with the file name.",
     "When the layout file is named menu_home all of the containing ids should be prefixed with menuHome to avoid ambiguity between different menu files across different menu items.",
@@ -32,11 +28,19 @@ class MatchingMenuIdDetector : ResourceXmlDetector() {
       val fix = fix()
           .replace()
           .text(id)
-          .with(fileName)
+          .with(toBeReplaced(fileName, id))
           .autoFix()
           .build()
 
       context.report(ISSUE_MATCHING_MENU_ID, attribute, context.getValueLocation(attribute), "Id should start with: $fileName", fix)
+    }
+  }
+
+  private fun toBeReplaced(fileName: String, id: String): String {
+    return if (id.startsWith(fileName, ignoreCase = true)) {
+      fileName + id.substring(fileName.length)
+    } else {
+      fileName + id.capitalize()
     }
   }
 }
