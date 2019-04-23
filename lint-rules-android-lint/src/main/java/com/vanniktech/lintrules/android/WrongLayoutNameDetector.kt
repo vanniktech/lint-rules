657ffb2b121e280f69c2f1ae1ffab59b54209685
Niklas Baudy <niklas.baudy@vanniktech.de>
2019-04-23 14:47:38 +0200

    WrongLayoutNameDetector: Don't concatenate resource prefix with allowed prefixes if they are equal. (#296)

--- lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/WrongLayoutNameDetector.kt
+++ lint-rules-android-lint/src/main/java/com/vanniktech/lintrules/android/WrongLayoutNameDetector.kt
@@ -21,9 +21,17 @@ val ISSUE_WRONG_LAYOUT_NAME = Issue.create("WrongLayoutName",
 
 class WrongLayoutNameDetector : LayoutDetector() {
   override fun visitDocument(context: XmlContext, document: Document) {
-    val modified = allowedPrefixes.map { context.project.resourcePrefix().forceUnderscoreIfNeeded() + it }
+    val modified = allowedPrefixes.map {
+      val resourcePrefix = context.project.resourcePrefix()
+          .forceUnderscoreIfNeeded()
+
+      if (resourcePrefix != it) resourcePrefix + it else it
+    }
+
     val doesNotStartWithPrefix = modified.none { context.file.name.startsWith(it) }
-    val notEquals = modified.map { it.dropLast(1) }.none { context.file.name == "$it.xml" }
+    val notEquals = modified.map {
+        it.dropLast(1) // Drop the trailing underscore.
+    }.none { context.file.name == "$it.xml" }
 
     if (doesNotStartWithPrefix && notEquals) {
       context.report(ISSUE_WRONG_LAYOUT_NAME, document, context.getLocation(document), "Layout does not start with one of the following prefixes: ${modified.joinToString()}")
