85332240209ceb4890f403214581d285b1bcd886
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-09-24 12:47:01 +0200

    Update Android Gradle Plugin to 7.3.0 & Lint to 30.3.0 (with Kotlin DSL)

---
+++ lint-rules-android/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+  id("com.android.library")
+  id("com.vanniktech.maven.publish")
+}
+
+android {
+  namespace = "com.vanniktech.lintrulesandroid"
+  compileSdk = libs.versions.compileSdk.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.minSdk.get().toInt()
+  }
+}
+
+dependencies {
+  lintPublish(project(":lint-rules-rxjava2-lint"))
+}
